e6bd2827425757253b038a3b852f1fc46b20e5eb
dhan04shree <dhanshreemagar0@gmail.com>
2025-06-05 21:22:01 +0530

    styling changes

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,20 +1,18 @@
 export default function HeroSection(){
     return(
-    <div class="h-screen flex items-center justify-center text-white bg-gray-900">
-        <div class="max-w-2xl text-center px-4">
-        <h1 class="text-5xl font-bold mb-6 leading-tight">
+    <div className="h-screen flex items-center justify-center text-white bg-gray-900">
+        <div className="max-w-2xl text-center px-4">
+        <h1 className="text-5xl font-bold mb-6 leading-tight">
         Every Solution Matters. Capture It With AlgoVoice.
         </h1>
-        <p class="text-lg text-[#ccc9dc] mb-6">
+        <p className="text-lg text-[#ccc9dc] mb-6">
             Record your logic, ideas, and breakthroughs in one place. Revisit your past work to sharpen your thinking and never lose progress.
         </p>
-        <button class="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
-        Get Started
+        <form action="/newentry">
+        <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">Get Started
         </button>
+        </form>
         </div>
-    </div>
-
-
-        
+    </div>        
     )
 }

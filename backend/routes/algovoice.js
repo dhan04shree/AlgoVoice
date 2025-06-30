c3ba6ca8efd8bcca80adb91936982231fee7dbe2
dhan04shree <dhanshreemagar0@gmail.com>
2025-06-30 16:11:49 +0530

    feat: add showEntry route

--- backend/routes/algovoice.js
+++ backend/routes/algovoice.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const entryController = require('../controller/entryController');
-
+const showEntry = require('../controller/entryController');
 router.post('/',entryController);
-
+router.get('/:id',showEntry)
 module.exports = router;
